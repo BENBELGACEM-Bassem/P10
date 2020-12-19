516f596784de5ee008a7c97a46fa72ce7ff12725
Bassem BEN BELGACEM <benblg.bassem@gmail.com>
2020-12-19 13:54:17 +0100

    add travis file

---
+++ purbeurre/settings/travis.py
@@ -0,0 +1,15 @@
+"""Data base configuration for travis"""
+
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
+    }
+}
